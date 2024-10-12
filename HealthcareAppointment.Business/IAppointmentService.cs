04336c2483d48cf4b1b345e1b39ca3457d710c58
minhkhai10122003 <136252548+khaioday@users.noreply.github.com>
2024-10-12 23:06:31 +0700

    Create IAppointmentService.cs

---
+++ HealthcareAppointment.Business/IAppointmentService.cs
@@ -0,0 +1,15 @@
+﻿using HealthcareAppointment.Models;
+
+namespace HealthcareAppointment.Business
+{
+    public interface IAppointmentService
+    {
+        Task<IEnumerable<Appointment>> GetAppointments();
+        Task<Appointment> GetAppointmentById(Guid id);
+        Task AddAppointment(Appointment appointment);
+        Task UpdateAppointment(Appointment appointment);
+        Task DeleteAppointment(Guid id);
+        Task CancelAppointment(Guid id);
+    }
+
+}
