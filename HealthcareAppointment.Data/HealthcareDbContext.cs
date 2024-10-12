25478338d44f226100e21f24412cd0da32ac54a2
minhkhai10122003 <136252548+khaioday@users.noreply.github.com>
2024-10-12 23:06:37 +0700

    Create HealthcareDbContext.cs

---
+++ HealthcareAppointment.Data/HealthcareDbContext.cs
@@ -0,0 +1,26 @@
+﻿using HealthcareAppointment.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HealthcareAppointment.Data
+{
+    public class HealthcareDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Appointment>()
+                .HasOne<User>(a => a.Patient)
+                .WithMany()
+                .HasForeignKey(a => a.PatientId);
+
+            modelBuilder.Entity<Appointment>()
+                .HasOne<User>(a => a.Doctor)
+                .WithMany()
+                .HasForeignKey(a => a.DoctorId);
+        }
+    }
+
+
+}
